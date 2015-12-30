32b0d16fee4d79995c9f5861766d52a049975027
Alicia Rodriguez Taboada <Alicia_Rodriguez@ultimatesoftware.com>
2015-12-30 17:05:28 -0500

    added the account.ui dependency

--- js/simple-todos-angular.js
+++ js/simple-todos-angular.js
@@ -1,8 +1,14 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+
+    /* Configure accounts UI to use usernames instead of email */
+    Accounts.ui.config({
+      passwordSignupFields: "USERNAME_ONLY"
+    });
+
   // This code only runs on the client
-    angular.module('simple-todos',['angular-meteor']);
+    angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
 
     angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
@@ -11,11 +17,13 @@ if (Meteor.isClient) {
         return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
       });
 
-      $scope.addTask = function (newTask) {
+      $scope.addTask = function(newTask) {
         $scope.tasks.push( {
-          text: newTask,
-          createdAt: new Date() }
-        );
+            text: newTask,
+            createdAt: new Date(),             // current time
+            owner: Meteor.userId(),            // _id of logged in user
+            username: Meteor.user().username  // username of logged in user
+          });
       };
 
       $scope.$watch('hideCompleted', function() {
